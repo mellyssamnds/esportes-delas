47e1c4af858eed072ae7b3b1a8f8c8f81cf1549b
mellyssamnds <mellyssah.mendes@live.com>
2024-09-01 05:41:09 -0300

    add swagger configuration and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AthleteModule } from './models/athlete.module';
+import { TrainingModule } from './models/training.module';
+import * as mongoose from 'mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+mongoose.set('debug', true);
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/?retryWrites=true&w=majority&appName=${process.env.MONGODB_DATABASE}`,
+    ),
+    AthleteModule,
+    TrainingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
