578969516551f03b0bf078d75d0c746906023ec6
mellyssamnds <mellyssah.mendes@live.com>
2024-09-01 05:41:52 -0300

    add description swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env);
+
+  const config = new DocumentBuilder()
+    .setTitle('Esportes dELAS')
+    .setDescription(
+      'Projeto Final da Imersão JavaScript {reprograma} que consiste em um sistema de empoderamento feminino na prática de atividades físicas em diferentes modalidades, promovendo um ambiente seguro e encorajador.',
+    )
+    .setVersion('1.0')
+    .addTag('athletes')
+    .addTag('trainings')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('home', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
